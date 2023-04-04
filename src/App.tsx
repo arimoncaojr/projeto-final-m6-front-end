a8326b52f9952c12302c51656eb86de3d8066a6d
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-04-04 13:38:39 -0300

    feat: esqueleto do projeto finalizado

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ToastContainer } from "react-toastify";
+import { RoutesMain } from "./routes";
+
+function App() {
+  return (
+    <>
+      <RoutesMain />
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
