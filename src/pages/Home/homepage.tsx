9a9dfa457611a1708d81dd1c6a2c2c0dc596da20
Thiago Rodrigues <thiago.rds@outlook.com.br>
2023-04-09 21:17:44 -0400

    fix: apagado código da homepage que estava sendo usado apenas para testes

--- src/pages/Home/homepage.tsx
+++ src/pages/Home/homepage.tsx
@@ -1,49 +1,11 @@
-import { Header } from "../../components/header/header"
-import { Input } from "../../components/input/input"
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from 'yup';
-
-interface FormData {
-     username: string;
-     email: string;
-     age: string;
-}
-
 export const HomePage = () => {
-     
-     const schema = yup.object().shape({
-          username: yup.string().required(),
-          email: yup.string().email().required(),
-          age: yup.number().required().positive().integer(),
-        });
 
-     const {register,handleSubmit, formState: { errors }} = useForm<FormData>({
-          resolver: yupResolver(schema),
-        });
-      
-        const onSubmit = (data:FormData) => {
-          console.log(data);
-        };
-     
-        const genders = [
-          { value: 'male', label: 'Male' },
-          { value: 'female', label: 'Female' },
-          { value: 'other', label: 'Other' },
-        ];
 
      return (
           <>
-               <Header/>
                <div className="container">
                 
-                    <form onSubmit={handleSubmit(onSubmit)}>
-
-                         <Input type="text" label="login" name="username" placeholder="Digite seu nome" register={register} errors={errors} />
-                         <Input type="text" label="login" name="email" placeholder="Digite seu Email" register={register} errors={errors} tipo="filter"/>
-                         <Input type="select" label="login" name="age" placeholder="Digite sua idade" options={genders} register={register} errors={errors} />
-                         <button type="submit">ok</button>
-                    </form>
+                   <h1>HOMEPAGE</h1>
                </div>
            
           </>
