c611af9e568aac338d903eb32a1b37fd9f5a84b2
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-04-13 14:15:00 -0300

    feat: imagem e frases principais

--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -1,8 +1,19 @@
+import { Footer } from "../../components/footer/footer";
+import { Header } from "../../components/header/header";
+import { Wrapper } from "../../styles/wrapper";
+import { ImgPrincipal, TextContainer, Title, Subtitle } from "./dashboardStyle";
 
 export const DashboardPage = () => {
-     return (
-          <div className="container">
-               <h1>Dashboard</h1>
-          </div>
-     )
-}
+  return (
+    <Wrapper>
+      <Header />
+      <ImgPrincipal>
+        <TextContainer>
+          <Title>Motors Shop</Title>
+          <Subtitle>A melhor plataforma de anúncios de carros do país</Subtitle>
+        </TextContainer>
+      </ImgPrincipal>
+      <Footer />
+    </Wrapper>
+  );
+};
--- src/styles/wrapper.ts
+++ src/styles/wrapper.ts
@@ -1,17 +1,16 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const WrapperGlobal = styled.div`
-     width: 100vw;
-     min-height: 100vh;
-     background-color: var(--gray-8);
-     overflow-wrap: break-word;
-`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: var(--gray-8);
+  overflow-wrap: break-word;
+`;
 export const Wrapper = styled.div`
-     min-height: 100vh;
-     display: flex;
-     flex-direction: column;
-     justify-content: space-between;
-     position: relative;
-     z-index:0;
-     
-`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+  z-index: 0;
+`;
