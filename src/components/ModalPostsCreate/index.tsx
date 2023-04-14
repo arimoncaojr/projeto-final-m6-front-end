877e48304d029731b2090d2b5afbe1100a55f89b
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-04-14 18:00:04 -0300

    feat: finalizado estilização formulário criar anuncio

--- src/components/ModalPostsCreate/index.tsx
+++ src/components/ModalPostsCreate/index.tsx
@@ -1,5 +1,6 @@
 import {
   ContainerModal,
+  ContentWrapper,
   FormModal,
   TitleAndButton,
   CloseBtn,
@@ -48,71 +49,73 @@ export const ModalPostsCreate = () => {
           <TitlePost>Criar anúncio</TitlePost>
           <CloseBtn>X</CloseBtn>
         </TitleAndButton>
-        <SubTitlePost>Informações do veículo</SubTitlePost>
-        <Label htmlFor="mark">Seleciona a Marca</Label>
-        <BigSelect id="mark">
-          <option value="">Selecione</option>
-          <option value="Mercedes Benz">Mercedes Benz</option>
-        </BigSelect>
-        <Label htmlFor="model">Selecione o Modelo</Label>
-        <BigSelect id="model">
-          <option value="">Selecione</option>
-          <option value="A 200 CGI ADVANCE SEDAN">
-            A 200 CGI ADVANCE SEDAN
-          </option>
-        </BigSelect>
-        <LabelAndFieldDiv>
-          <LabelAndInputWrapper>
-            <Label htmlFor="year">Ano</Label>
-            <Label htmlFor="fuelType">Selecione o Combustível</Label>
-          </LabelAndInputWrapper>
-          <LabelAndInputWrapper>
-            <SmallInput id="year" placeholder="ex: 2018" />
-            <SmallSelect id="fuelType">
-              <option value="">Selecione</option>
-              <option value="flex">Flex</option>
-              <option value="hibrido">Hibrido</option>
-              <option value="eletrico">Elétrico</option>
-            </SmallSelect>
-          </LabelAndInputWrapper>
-        </LabelAndFieldDiv>
-        <LabelAndFieldDiv>
-          <LabelAndInputWrapper>
-            <Label htmlFor="kilometers">Quilometragem</Label>
-            <Label htmlFor="color">Selecione a Cor</Label>
-          </LabelAndInputWrapper>
-          <LabelAndInputWrapper>
-            <SmallInput id="kilometers" placeholder="ex: 30.000" />
-            <SmallSelect id="color">
-              <option value="azul">Azul</option>
-              <option value="branco">Branco</option>
-              <option value="preto">Preto</option>
-            </SmallSelect>
-          </LabelAndInputWrapper>
-        </LabelAndFieldDiv>
-        <LabelAndFieldDiv>
-          <LabelAndInputWrapper>
-            <Label htmlFor="tablePriceFiper">Preço tabela FIPE</Label>
-            <Label htmlFor="price">Preço</Label>
-          </LabelAndInputWrapper>
-          <LabelAndInputWrapper>
-            <SmallInput id="tablePriceFiper" readOnly />
-            <SmallInput id="price" placeholder="ex: R$50.000" />
-          </LabelAndInputWrapper>
-        </LabelAndFieldDiv>
-        <Label htmlFor="description">Descrição</Label>
-        <TextArea></TextArea>
-        <Label htmlFor="imageCap"> Imagem da capa</Label>
-        <BigInput id="imgCap" />
-        <Label htmlFor="firstImage"> 1° Imagem da galeria</Label>
-        <BigInput id="firstImage" />
-        <Label htmlFor="secondImage">2° Imagem da galeria</Label>
-        <BigInput id="secondImage" />
-        <AddImageBtn>Adicionar campo para imagem da galeria</AddImageBtn>
-        <DivFinalBtns>
-          <CancelBtn>Cancelar</CancelBtn>
-          <CreatePostBtn>Criar anúncio</CreatePostBtn>
-        </DivFinalBtns>
+        <ContentWrapper>
+          <SubTitlePost>Informações do veículo</SubTitlePost>
+          <Label htmlFor="mark">Marca</Label>
+          <BigSelect id="mark">
+            <option value="">Selecione</option>
+            <option value="Mercedes Benz">Mercedes Benz</option>
+          </BigSelect>
+          <Label htmlFor="model">Modelo</Label>
+          <BigSelect id="model">
+            <option value="">Selecione</option>
+            <option value="A 200 CGI ADVANCE SEDAN">
+              A 200 CGI ADVANCE SEDAN
+            </option>
+          </BigSelect>
+          <LabelAndFieldDiv>
+            <LabelAndInputWrapper>
+              <Label htmlFor="fuelType">Combustível</Label>
+              <SmallSelect id="fuelType">
+                <option value="">Selecione</option>
+                <option value="flex">Flex</option>
+                <option value="hibrido">Hibrido</option>
+                <option value="eletrico">Elétrico</option>
+              </SmallSelect>
+            </LabelAndInputWrapper>
+            <LabelAndInputWrapper>
+              <Label htmlFor="year">Ano</Label>
+              <SmallInput id="year" placeholder="ex: 2018" />
+            </LabelAndInputWrapper>
+          </LabelAndFieldDiv>
+          <LabelAndFieldDiv>
+            <LabelAndInputWrapper>
+              <Label htmlFor="color">Cor</Label>
+              <SmallSelect id="color">
+                <option value="azul">Azul</option>
+                <option value="branco">Branco</option>
+                <option value="preto">Preto</option>
+              </SmallSelect>
+            </LabelAndInputWrapper>
+            <LabelAndInputWrapper>
+              <Label htmlFor="kilometers">Quilometragem</Label>
+              <SmallInput id="kilometers" placeholder="ex: 30.000" />
+            </LabelAndInputWrapper>
+          </LabelAndFieldDiv>
+          <LabelAndFieldDiv>
+            <LabelAndInputWrapper>
+              <Label htmlFor="tablePriceFiper">Preço tabela FIPE</Label>
+              <SmallInput id="tablePriceFiper" readOnly />
+            </LabelAndInputWrapper>
+            <LabelAndInputWrapper>
+              <Label htmlFor="price">Preço</Label>
+              <SmallInput id="price" placeholder="ex: R$50.000" />
+            </LabelAndInputWrapper>
+          </LabelAndFieldDiv>
+          <Label htmlFor="description">Descrição</Label>
+          <TextArea></TextArea>
+          <Label htmlFor="imageCap"> Imagem da capa</Label>
+          <BigInput id="imgCap" />
+          <Label htmlFor="firstImage"> 1° Imagem da galeria</Label>
+          <BigInput id="firstImage" />
+          <Label htmlFor="secondImage">2° Imagem da galeria</Label>
+          <BigInput id="secondImage" />
+          <AddImageBtn>Adicionar campo para imagem da galeria</AddImageBtn>
+          <DivFinalBtns>
+            <CancelBtn>Cancelar</CancelBtn>
+            <CreatePostBtn>Criar anúncio</CreatePostBtn>
+          </DivFinalBtns>
+        </ContentWrapper>
       </FormModal>
     </ContainerModal>
   );
