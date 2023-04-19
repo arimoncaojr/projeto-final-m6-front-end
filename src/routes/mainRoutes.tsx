eb5d06a6ac947f8c6332df1ca54e36a4667656b9
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-04-19 20:47:49 -0300

    fix: routes

--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -3,13 +3,12 @@ import { DashboardPage } from "../pages/Dashboard/dashboard";
 import { HomePage } from "../pages/Home/homepage";
 import { LoginPage } from "../pages/Login/login";
 import { RegisterPage } from "../pages/Register/register";
-import { ModalPostsCreate } from "../components/ModalPostsCreate";
 
 export const RoutesMain = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<ModalPostsCreate />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
