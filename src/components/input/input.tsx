9e6ca7033ac0c51ace090a9ed1b009902d1886e3
Pedro Mendes <106615792+pedroMendes182@users.noreply.github.com>
2023-05-10 09:12:46 -0300

    merge: develop

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,103 +1,98 @@
 import { UseFormRegister } from "react-hook-form";
-import { WrapperStyle, InputStyle, LabelStyle, SelectStyle, InputMaskStyle, TextAreaStyle } from "./inputStyle";
+import {
+  WrapperStyle,
+  InputStyle,
+  LabelStyle,
+  SelectStyle,
+  InputMaskStyle,
+  TextAreaStyle,
+} from "./inputStyle";
 
 interface ISelectOption {
-     value: string;
-     label: string;
+  value: string;
+  label: string;
 }
 
 interface IInputProps {
-     type: string;
-     label?: string;    
-     name: string;
-     placeholder: string;
-     register: UseFormRegister<any>;
-     errors: Record<string, any>;
-     options?: ISelectOption[];
-     typeStyled?: "filter" | "tel";
-     disabled?: boolean;
-     onChange?: React.ChangeEventHandler<HTMLInputElement>;
-     value?: string
-     maxLength?:number;
+  type: string;
+  label?: string;
+  name: string;
+  placeholder: string;
+  register: UseFormRegister<any>;
+  errors: Record<string, any>;
+  options?: ISelectOption[];
+  typeStyled?: "filter" | "tel";
+  disabled?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  value?: string;
+  maxLength?: number;
 }
-
 
 export const Input = ({
-     type,
-     label,
-     name,
-     placeholder,
-     register,
-     errors,
-     options,
-     typeStyled,
-     onChange,
-     value,
-     maxLength }: IInputProps) => {
-   
+  type,
+  label,
+  name,
+  placeholder,
+  register,
+  errors,
+  options,
+  typeStyled,
+  onChange,
+  value,
+  maxLength,
+}: IInputProps) => {
+  return (
+    <WrapperStyle>
+      {label && <LabelStyle htmlFor={name}>{label}</LabelStyle>}
+      {type === "select" ? (
+        <SelectStyle id={name} {...register(name)}>
+          {options?.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </SelectStyle>
+      ) : typeStyled === "tel" ? (
+        <InputMaskStyle
+          mask="(99) 9 9999-9999"
+          id={name}
+          type={type}
+          placeholder={placeholder}
+          {...register(name)}
+          className="input_mask"
+        />
+      ) : name === "cpf" ? (
+        <InputMaskStyle
+          mask="999.999.999-99"
+          id={name}
+          type={type}
+          placeholder={placeholder}
+          {...register(name)}
+          className="input_mask"
+        />
+      ) : type === "textarea" ? (
+        <TextAreaStyle
+          id={name}
+          placeholder={placeholder}
+          {...register(name)}
+          maxLength={200}
+        />
+      ) : (
+        <InputStyle
+          id={name}
+          type={type}
+          placeholder={placeholder}
+          {...register(name)}
+          typeStyled={typeStyled}
+          onChange={onChange}
+          value={value}
+          maxLength={maxLength}
+        />
+      )}
 
-     return (
-          <WrapperStyle>
-               {label && <LabelStyle htmlFor={name}>{label}</LabelStyle>}
-               {type === 'select' ?
-                         (
-                              <SelectStyle id={name} {...register(name)}>
-                                   {options?.map((option) => (
-                                        <option key={option.value} value={option.value}>
-                                             {option.label}
-                                        </option>
-                                   ))}
-                              </SelectStyle>
-                         )
-                    : typeStyled === "tel" ?
-                         (
-                              <InputMaskStyle
-                                   mask="(99) 9 9999-9999"
-                                   id={name}
-                                   type={type}
-                                   placeholder={placeholder}
-                                   {...register(name)}
-                                   className="input_mask"
-                              />
-                           
-                         )
-                    : name === "cpf" ?
-                         (
-                              <InputMaskStyle
-                                   mask="999.999.999-99"
-                                   id={name}
-                                   type={type}
-                                   placeholder={placeholder}
-                                   {...register(name)}
-                                   className="input_mask"
-                              />     
-                         )        
-                    : type === "textarea" ? 
-                         (
-                              <TextAreaStyle
-                                   id={name}  
-                                   placeholder={placeholder}        
-                                   {...register(name)}
-                                   maxLength={200}          
-                              />                   
-
-                         )
-                    :               
-                         (
-                              <InputStyle
-                                   id={name}
-                                   type={type}
-                                   placeholder={placeholder}
-                                   {...register(name)}
-                                   typeStyled={typeStyled}
-                                   onChange={onChange}
-                                   value={value} 
-                                   maxLength={maxLength}          
-                              />
-                         )
-               }
-
-               {errors[name] && <span className="messageErrors">{errors[name].message}</span>}
-        </WrapperStyle>
-     )
-}
+      {errors[name] && (
+        <span className="messageErrors">{errors[name].message}</span>
+      )}
+    </WrapperStyle>
+  );
+};
